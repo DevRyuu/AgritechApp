31c23a50fdd0e1433c5e9237473a87a1864964b7
jas <jasper.r0328@gmail.com>
2024-02-29 17:08:50 +0900

    초기 커밋.

---
+++ AgritechApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AgritechApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AgritechApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AgritechApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
